b819fd749f261dfc608c5ac55f1f13d431218aaf
thangvu <vthangvu98@gmail.com>
2025-10-01 20:18:05 -0400

    Init

--- src/main/java/com/thangv/SoccerFixturesApp/repository/NotificationReceiptRepository.java
+++
@@ -1,8 +0,0 @@
-package com.thangv.SoccerFixturesApp.repository;
-
-import com.thangv.SoccerFixturesApp.entity.NotificationReceipt;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NotificationReceiptRepository extends JpaRepository<NotificationReceipt, Long> {
-    boolean existsByUserIdAndFixtureIdAndKindAndChannel(Long userId, Long fixtureId, String kind, String channel);
-}
